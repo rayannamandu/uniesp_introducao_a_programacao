5c14eeb6aabf958b8d47a00e8284d59e0fee6dd2
Rayanna Mandú <112412163+rayannamandu@users.noreply.github.com>
2022-11-29 10:59:39 -0300

    Create saudacao.py

---
+++ Aula06/atividades/saudacao.py
@@ -0,0 +1,11 @@
+'''
+5. Utilizando a lista do exercício anterior, exiba
+uma saudação ("Olá como vai você"), personalizado
+com o nome de cada amigo. A saudação deve ser a mesma,
+alterando apenas o nome do amigo.
+'''
+
+amigos = ["Endi", "Felipe", "João", "Leonardo", "Maria"];
+
+for amigo in amigos:
+    print(f'Olá {amigo}, como vai você?');
